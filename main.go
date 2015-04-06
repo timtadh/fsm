66a6b59f90e2212557ec3dbc644f8b3e50685e05
Tim Henderson <tim.tadh@gmail.com>
2015-04-06 18:35:29 -0400

    refactored output
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ package main
  */
 
 import (
+	"bytes"
 	"compress/gzip"
 	"encoding/hex"
 	"fmt"
@@ -40,6 +41,8 @@ import (
 
 import (
 	"github.com/timtadh/getopt"
+	"github.com/timtadh/fs2/bptree"
+	"github.com/timtadh/fs2/fmap"
 )
 
 import (
@@ -67,7 +70,7 @@ fsm - frequent subgraph mine the graph(s)
 
 Options
     -h, --help                          print this message
-    -o, --output=<path>                 path to output file
+    -o, --output=<path>                 path to output directory
     -s, --support=<int>                 number of unique embeddings (required)
     -m, --min-vertices=<int>            minimum number of nodes to report
                                         (5 by default)
@@ -185,7 +188,14 @@ func ParseInt(str string) int {
 func AssertDir(dir string) string {
 	dir = path.Clean(dir)
 	fi, err := os.Stat(dir)
-	if err != nil {
+	if err != nil && os.IsNotExist(err) {
+		err := os.MkdirAll(dir, 0775)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+			Usage(ErrorCodes["baddir"])
+		}
+		return dir
+	} else if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 		Usage(ErrorCodes["baddir"])
 	}
@@ -239,13 +249,13 @@ func main() {
 	memCache := false
 	memProfile := ""
 	cpuProfile := ""
-	output := ""
+	outputDir := ""
 	for _, oa := range optargs {
 		switch oa.Opt() {
 		case "-h", "--help":
 			Usage(0)
 		case "-o", "--output":
-			output = AssertFile(oa.Arg())
+			outputDir = AssertDir(oa.Arg())
 		case "-s", "--support":
 			support = ParseInt(oa.Arg())
 		case "-m", "--min-vertices":
@@ -266,7 +276,7 @@ func main() {
 		Usage(ErrorCodes["opts"])
 	}
 
-	if output == "" {
+	if outputDir == "" {
 		fmt.Fprintf(os.Stderr, "You must supply an output file (use -o)\n")
 		Usage(ErrorCodes["opts"])
 	}
@@ -348,14 +358,86 @@ func main() {
 		defer f.Close()
 	}
 
-	out := store.NewFs2BpTree(G, output)
+	allPath := path.Join(outputDir, "all.bptree")
+	labelsPath := path.Join(outputDir, "labels.bptree")
+	// closedPath := path.Join(outputDir, "closed.dot")
+
+	all := store.NewFs2BpTree(G, allPath)
+	labelsBf, err := fmap.CreateBlockFile(labelsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	labels, err := bptree.New(labelsBf, -1, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(labels)
 
 	for psg := range mine.Mine(G, support, minVert, maker, memProfFile) {
-		out.Add(psg.Sg.ShortLabel(), psg)
+		all.Add(psg.Sg.ShortLabel(), psg)
 	}
 
-	for key, next := out.Keys()(); next != nil; key, next = next() {
+	/*
+	for key, next := all.Keys()(); next != nil; key, next = next() {
 		fmt.Println(hex.EncodeToString(key))
 	}
+	*/
+
+	/*
+	var cur []byte
+	for key, _, next := all.Backward()(); next != nil; key, _, next = next() {
+		if !bytes.Equal(key, cur) {
+			fmt.Println(hex.EncodeToString(key))
+		}
+		cur = key
+	}
+	*/
+
+	var cur []byte
+	var had bool = false
+	for key, psg, next := all.Backward()(); next != nil; key, psg, next = next() {
+		if cur != nil && !bytes.Equal(key, cur) {
+			if !had {
+				had = false
+				// output key here
+				doOutput(all, cur)
+			}
+			addToLabels(labels, cur)
+		}
+		has, err := labels.Has(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		had = had || has
+		cur = key
+		addToLabels(labels, psg.Parent)
+	}
+	if !had && cur != nil {
+		// output key here
+		doOutput(all, cur)
+	}
+}
+
+func addToLabels(labels *bptree.BpTree, label []byte) {
+	has, err := labels.Has(label)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !has {
+		err = labels.Add(label, []byte{0})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func doOutput(all store.SubGraphs, key []byte) {
+	fmt.Println("//", hex.EncodeToString(key))
+	fmt.Println()
+	for _, psg, next := all.Find(key)(); next != nil; _, psg, next = next() {
+		fmt.Println(psg.Sg)
+	}
+	fmt.Println()
+	fmt.Println()
 }
 
